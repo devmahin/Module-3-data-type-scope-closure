3adfd87f0a7633536f5fbc0478c807755aebc96f
devmahin <mahinhowlader123@gmail.com>
2024-02-23 22:57:06 +0600

    data type

---
+++ script.js
@@ -0,0 +1,12 @@
+let d = [30,40,50,60,70];
+let b = d;
+b = [23,4,5]
+console.log(d);
+console.log(b);
+
+
+let obj = {name : "mahin"}
+let obj2 = obj;
+obj2 = {name : "antor"}
+console.log(obj)
+console.log(obj2)
