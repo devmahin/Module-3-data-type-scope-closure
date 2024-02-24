3356440a284096643428e06a09bed5843fcc41a7
devmahin <mahinhowlader123@gmail.com>
2024-02-24 16:04:46 +0600

    hoisting

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let d = [30,40,50,60,70];
-let b = d;
-b = [23,4,5]
-console.log(d);
-console.log(b);
+// let d = [30,40,50,60,70];
+// let b = d;
+// b = [23,4,5]
+// // console.log(d);
+// // console.log(b);
 
 
-let obj = {name : "mahin"}
-let obj2 = obj;
-obj2 = {name : "antor"}
-console.log(obj)
-console.log(obj2)
+// let obj = {name : "mahin"}
+// let obj2 = obj;
+// obj2 = {name : "antor"}
+// // console.log(obj)
+// // console.log(obj2)
+
+
+
+let a =  "0";
+
+if(a){
+    console.log("Truthy value")
+}else{
+    console.log("Falsy value")
+}
+
+const nameMAHUN= [3434]
+// console.log(nameMAHUN)
